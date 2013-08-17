94cc5874ba50df9359fcdc1b1f1fc3af18ad291b
dustcoin <dustcoin@gmail.com>
2013-08-17 16:24:43 -0700

    Add markers from geocash API.

--- GeoCash/src/com/example/geocash/MainActivity.java
+++ GeoCash/src/com/example/geocash/MainActivity.java
@@ -1,5 +1,25 @@
 package com.example.geocash;
 
+import java.io.IOException;
+
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.FragmentManager;
@@ -10,6 +30,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.content.Context;
 import android.util.Log;
+
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
@@ -74,8 +95,52 @@ public class MainActivity extends Activity implements LocationListener	{
 	            googleMap.moveCamera(center);
 	            googleMap.animateCamera(zoom);
         }
+        new GetLocationsTask().execute();
     }
-        
+
+	private class GetLocationsTask extends AsyncTask<Void, Void, JSONArray> {
+		@Override
+		protected JSONArray doInBackground(Void... params) {
+	        HttpGet httpGet = new HttpGet("http://geocash.dustcoin.com/api/locations");
+	        HttpClient httpClient = new DefaultHttpClient();
+	        String jsonString = null;
+	        try {
+				HttpResponse response = httpClient.execute(httpGet);
+				jsonString = EntityUtils.toString(response.getEntity());
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        JSONArray json = null;
+			try {
+				json = new JSONArray(jsonString);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return json;
+		}
+		@Override
+		protected void onPostExecute(JSONArray json) {
+	        // Add markers for coin locations
+			try {
+	        	for (int i = 0; i < json.length(); i++) {
+	        		JSONObject loc = json.getJSONObject(i);
+	        		myPosition = new LatLng(loc.getDouble("lat"), loc.getDouble("lon"));
+	    	        
+	    	        googleMap.addMarker(new MarkerOptions().position(myPosition).title(loc.getString("name")
+	    	        		+ ": " + (loc.getBoolean("claimed") ? "Claimed" : "Available!")));
+	        	}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	@Override
 	public void onLocationChanged(Location location) {
 	    // TODO Auto-generated method stub
